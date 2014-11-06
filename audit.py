f40f55019fad64f03618e9a11b2c0974899da8e4
unknown <cpwd@cpwdlap14.STUDENT.AVRTECH.COM>
2014-11-06 13:51:53 -0500

    Added audit.py

---
+++ audit.py
@@ -0,0 +1,43 @@
+import os, logger, config, sys
+
+log = logger.Logger(config.data,__name__)
+def readBackups(loc):
+	retVal = False
+	backups = {}
+	log.log("Attempting to read backups from {}".format(loc),0)
+
+	try:
+		for f in os.listdir(loc):
+			age = os.path.getctime(f)
+			backups[f] = age
+
+		log.log("Backups successfully read.",0)
+		retVal = backups
+	except:
+		log.log("The follow exception has been caught: ")
+
+
+
+	return(retVal)
+
+
+def checkBackups(loc, maxage):
+	retVal = False
+	backups = readBackups(loc)
+	obselete_backups = []
+
+	log.log("Checking for obselete backups.",0)
+	for backup in backups:
+		if backup => maxage:
+			obselete_backups.append(backup)
+
+	if len(obselete_backups) > 0:
+		log.log("The following backups are obselete and have been marked for deletion: {}".format(obselete_backups),0)
+	else:
+		log.log("No obselete backups found.",0)
+
+
+
+
+
+
